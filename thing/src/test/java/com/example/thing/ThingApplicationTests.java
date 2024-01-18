ebce59680176779d8976238ef67cb1d766c803e8
Vladimir Sabo <vladimir.sabo@nordclan.com>
2024-01-18 13:01:56 +0300

    Добавить два тестовых модуля; добавить докерфайлы для тестовых микросервисов, докер компоуз с БД для тестовых микросервисов;

---
+++ thing/src/test/java/com/example/thing/ThingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
