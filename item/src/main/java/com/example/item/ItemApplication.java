f64a4fa3eaf616e4c85d3d3d1c821f59946c6d3d
Vladimir Sabo <vladimir.sabo@nordclan.com>
2024-01-24 14:15:09 +0300

    добавить keycloak в compose.yml & item-service; убрать тестовую конфигурацию в item-service; сменить порт на thing-service

--- item/src/main/java/com/example/item/ItemApplication.java
+++ item/src/main/java/com/example/item/ItemApplication.java
@@ -1,15 +1,12 @@
 package com.example.item;
 
-import com.example.item.config.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @RefreshScope
-@EnableConfigurationProperties({Configuration.class})
 @EnableFeignClients
 public class ItemApplication {
 
